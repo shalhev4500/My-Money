782da0f4ba79f9e12844b3508f007c2d1af0308b
shalhev4500 <shalav4500@gmail.com>
2026-09-15 00:15:25 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zkhqpqrrwcfbkhkchuie.supabaseco'
+const supabaseUrl = 'https://zkhqpqrrwcfbkhkchuie.supabase.co'
 const supabaseAnonKey = 'sb_publishable_uKK4OfkJDe0OI46aPW2M9g_5rhYgmGS'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
